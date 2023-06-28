4e57b58a762691d6ffb0d37421cf28d1f36dd619
stanislavdrozd <stanislav.drozd@digitech7.by>
2023-06-28 16:43:13 +0300

    Finished homework.

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ const express = require("express");
 const webserver = express();
 
 webserver.use("/jsFiles", express.static(__dirname + "/public"));
+webserver.use(express.json());
 
 const port = 3000;
+
+const html = `<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport content=width=device-width, initial-scale=1.0">
+ <title>Document</title>
+ <script src="./jsFiles/script.js" type="text/javascript"></script>
+</head>
+<body>
+ <h1>Homework 2</h1>
+ <div class="buttons"></div>
+ <br>
+ <div class="stats"></div>
+</body>
+</html>`;
 
 const voteVariants = [
   {
@@ -21,21 +37,23 @@ const voteVariants = [
   },
 ];
 
+const voteStatistics = new Map();
+
 webserver.get("/", (req, res) => {
-  res.send(
-    `<html lang="en">
-     <head>
-      <meta charset="UTF-8">
-      <meta name="viewport content=width=device-width, initial-scale=1.0">
-      <title>Document</title>
-      <script src="./jsFiles/script.js" type="text/javascript"></script>
-     </head>
-    <body>
-      <h1>Homework 2</h1>
-      <div class="buttons"></div>
-    </body>
-    </html>`
-  );
+  res.send(html);
+});
+
+webserver.post("/vote", (req, res) => {
+  const voteOptionName = voteVariants.find(
+    (item) => item.code === req.body.code
+  ).option;
+  const previousValue = voteStatistics.get(voteOptionName) ?? 0;
+  voteStatistics.set(voteOptionName, previousValue + 1);
+  res.send(200);
+});
+
+webserver.get("/stats", (req, res) => {
+  res.send({ stats: [...voteStatistics] });
 });
 
 webserver.get("/variants", (req, res) => {
--- public/script.js
+++ public/script.js
@@ -1,16 +1,37 @@
-console.log(1);
+const handleVote = (code) => {
+  fetch("/vote", {
+    method: "POST",
+    body: JSON.stringify({
+      code: Number(code),
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+};
 
-document.addEventListener("DOMContentLoaded", () => {
-  let variants = [];
-  const buttons = document.querySelector(".buttons");
-  fetch("/variants")
-    .then((rawRes) => rawRes.json())
-    .then((res) => {
-      let buttonsHtml = res
-        .map((item) => {
-          return `<button>${item.option} - ${item.code}</button>`;
-        })
-        .join("");
-      buttons.innerHTML = buttonsHtml;
-    });
+document.addEventListener("DOMContentLoaded", async () => {
+  const statsButton = document.querySelector(".statsButton");
+  const buttonsBlock = document.querySelector(".buttons");
+  const result = await fetch("/variants");
+  const parsedResult = await result.json();
+  let buttonsHtml = parsedResult
+    .map((item) => {
+      return `<button>${item.option} - ${item.code}</button>`;
+    })
+    .join("");
+  buttonsBlock.innerHTML = buttonsHtml;
+
+  buttonsBlock.addEventListener("click", async (e) => {
+    await handleVote(e.target.innerText.split("- ")[1]);
+    const result = await fetch("/stats");
+    const parsedResult = await result.json();
+    const statsBlock = document.querySelector(".stats");
+    let statsHtml = parsedResult.stats
+      .map((item) => {
+        return `<span>${item[0]} - ${item[1]}</span><br>`;
+      })
+      .join("");
+    statsBlock.innerHTML = statsHtml;
+  });
 });

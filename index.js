a939cb0719f4166d83a5e12df7e454db48cb146e
stanislavdrozd <stanislav.drozd@digitech7.by>
2023-06-30 11:59:58 +0300

    Moved data to files.

--- index.js
+++ index.js
@@ -1,62 +1,42 @@
 const express = require("express");
-
+const fs = require("fs");
 const webserver = express();
 
-webserver.use("/jsFiles", express.static(__dirname + "/public"));
+webserver.use("/publicFiles", express.static(__dirname + "/public"));
 webserver.use(express.json());
 
 const port = 80;
 
-const html = `<html lang="en">
-<head>
- <meta charset="UTF-8">
- <meta name="viewport content=width=device-width, initial-scale=1.0">
- <title>Document</title>
- <script src="./jsFiles/script.js" type="text/javascript"></script>
-</head>
-<body>
- <h1>Homework 2</h1>
- <div class="buttons"></div>
- <br>
- <div class="stats"></div>
-</body>
-</html>`;
-
-const voteVariants = [
-  {
-    option: "JavaScript",
-    code: 1,
-  },
-  {
-    option: "PHP",
-    code: 2,
-  },
-  {
-    option: "Python",
-    code: 3,
-  },
-];
-
 const voteStatistics = new Map();
 
 webserver.get("/", (req, res) => {
-  res.send(html);
+  fs.readFile(__dirname + "/public/markup.html", (error, data) => {
+    if (error) throw error;
+    res.write(data);
+    res.end();
+  });
 });
 
 webserver.post("/vote", (req, res) => {
-  const voteOptionName = voteVariants.find(
-    (item) => item.code === req.body.code
-  ).option;
-  const previousValue = voteStatistics.get(voteOptionName) ?? 0;
-  voteStatistics.set(voteOptionName, previousValue + 1);
-  res.send({ stats: [...voteStatistics] });
+  fs.readFile(__dirname + "/public/voteVariants.json", (error, data) => {
+    if (error) throw error;
+    const voteOptionName = JSON.parse(data).find(
+      (item) => item.code === req.body.code
+    ).option;
+    const previousValue = voteStatistics.get(voteOptionName) ?? 0;
+    voteStatistics.set(voteOptionName, previousValue + 1);
+    res.send({ stats: [...voteStatistics] });
+  });
 });
 
 webserver.get("/variants", (req, res) => {
-  res.send(voteVariants);
+  fs.readFile(__dirname + "/public/voteVariants.json", (error, data) => {
+    if (error) throw error;
+    res.write(data);
+    res.end();
+  });
 });
 
 webserver.listen(port, () => {
   console.log("web server running on port " + port);
 });
-
